455e1dea3f30a54889f4a7da8081c62474f94121
smartcam3d <florian.schnoes@gmail.com>
2022-08-29 09:48:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of points in spiral", 1, 50000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 200, 9)
 
     Point = namedtuple('Point', 'x y')
     data = []
@@ -34,5 +34,5 @@ with st.echo(code_location='below'):
         data.append(Point(x, y))
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
+        .mark_circle(color='#0068c9', opacity=0.3)
         .encode(x='x:Q', y='y:Q'))
